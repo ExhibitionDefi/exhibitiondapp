45fc08bdc078e15d1a0c40e84e09b54049df592c
ExhibitionDefi <exhibitiondefi@gmail.com>
2026-05-19 02:35:28 +0100

    fix: remove api/rpc proxy, use direct RPC URL for wallet connection

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,18 +1,21 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { WagmiProvider } from './WagmiProvider';
 import { QueryProvider } from './QueryProvider';
-import { type State } from 'wagmi';
-import { RainbowKitProvider } from './RainbowKitProvider';
+
+const RainbowKitProvider = dynamic(
+  () => import('./RainbowKitProvider').then(m => m.RainbowKitProvider),
+  { ssr: false }
+);
 
 interface ProvidersProps {
   children: React.ReactNode;
-  initialState?: State;  
 }
 
-export function Providers({ children, initialState }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps) {
   return (
-    <WagmiProvider initialState={initialState}>  
+    <WagmiProvider>
       <QueryProvider>
         <RainbowKitProvider>
           {children}
